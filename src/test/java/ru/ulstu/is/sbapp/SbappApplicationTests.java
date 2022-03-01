1060e03bbb58e31598fe4dc6b79e38e31ccd8b69
Svetlnkk <89974865+Svetlnkk@users.noreply.github.com>
2022-03-01 19:44:24 +0400

    Laba2 internet prog

--- src/test/java/ru/ulstu/is/sbapp/SbappApplicationTests.java
+++ src/test/java/ru/ulstu/is/sbapp/SbappApplicationTests.java
@@ -1,13 +1,30 @@
 package ru.ulstu.is.sbapp;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ru.ulstu.is.sbapp.array.service.ArrayService;
 
 @SpringBootTest
 class SbappApplicationTests {
+	@Autowired
+	ArrayService arrayService;
 
 	@Test
-	void contextLoads() {
+	void testArrayNorm(){
+		final String res = arrayService.runArray(5, 5, "ArrayNorm");
+        Assertions.assertEquals("Привет Мир!", res);
 	}
+	@Test
+	void testArrayTrans(){
+		final String res = arrayService.runArray(5, 5, "ArrayTrans");
+        Assertions.assertEquals("Привет Мир!", res);
+	}
+	@Test
+    void testArrayErrorWired() {
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> arrayService.runArray(5, 5, "ArrayNorm"));
+    }
 
 }
